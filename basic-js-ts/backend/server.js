f353399a9f799079152b13952ab68662737975e4
Kansank <50160249+Kansank@users.noreply.github.com>
2020-11-10 13:46:57 +0700

    Update server.js

--- basic-js-ts/backend/server.js
+++ basic-js-ts/backend/server.js
@@ -1,12 +1,18 @@
 import express from 'express';
-
+import jokeRouter from './routers/jokeRouter.js';
+import mongoose from 'mongoose';
 
 
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true}));
+mongoose.connect(process.env.MONGODB_URL || 'mongodb://localhost/joke', {
+  useNewUrlParser:true,
+  useUnifiedTopology:true,
+  useCreateIndex:true,
+});
 
-
+app.use('/api/jokes', jokeRouter);
 app.use((err, req, res, next) => {
   res.status(500).send({message: err.message });
 });
