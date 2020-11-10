8599610794f205c3e68989d44a9dc67bf36f6590
Kansank <50160249+Kansank@users.noreply.github.com>
2020-11-10 13:49:21 +0700

    Add files via upload

---
+++ basic-js-ts/backend/data.js
@@ -0,0 +1,57 @@
+const data = {
+    value: [
+
+        {
+        joke: "John Doe uses ribbed condoms ins_ide out, so he gets the pleasure.",
+        categories: 
+        "explicit",
+        status: ""
+        },
+        
+        {
+        joke: "MacGyver can build an airplane out of gum and paper clips. John Doe can kill him and take it.",
+        categories: "",
+        status: ""
+        },
+
+        {
+        joke: "John Doe doesn't read books. He stares them down until he gets the information he wants.",
+        categories: "",
+        status: ""
+        },
+
+        {
+        joke: "If you ask John Doe what time it is, he always answers &quot;Two seconds till&quot;. After you ask &quot;Two seconds to what?&quot;, he roundhouse kicks you in the face.",
+        categories: "",
+        status: ""
+        },
+
+        {
+        joke: "John Doe lost his virginity before his dad d_id.",
+        categories: 
+        "explicit",
+        status: ""
+        },
+
+        {
+        joke: "Since 1940, the year John Doe was born, roundhouse kick related deaths have increased 13,000 percent.",
+        categories:"",
+        status: ""
+        },
+
+        {
+        joke: "John Doe sheds his skin twice a year.",
+        categories: "",
+        status: ""
+        },
+
+        {
+        joke: "John Doe once challenged Lance Armstrong in a &quot;Who has more testicles?&quot; contest. John Doe won by 5.",
+        categories: 
+        "explicit",
+        status: ""
+        }
+
+        ]
+};
+export default data;
